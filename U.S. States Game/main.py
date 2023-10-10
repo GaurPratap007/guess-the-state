ada8329785729bc038c485b6453e9a0a1848eb75
GaurPratap007 <103279493+GaurPratap007@users.noreply.github.com>
2023-10-10 21:07:34 +0530

    Add files via upload

---
+++ U.S. States Game/main.py
@@ -0,0 +1,40 @@
+import turtle
+import pandas
+import time
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+states = data.state.to_list()
+
+guessed_states=[]
+
+# Getting map Co-Ordinates
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+# Countdown Timer Clock
+
+while len(guessed_states)<50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Guessed", prompt="What's the name of another State: ").title()
+
+    if answer_state in states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+
+
+
+
+screen.exitonclick()
